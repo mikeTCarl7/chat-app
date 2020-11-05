ba4e1c15f5e52a4bc585814246d783b5b0661015
Mike Carl <mikecarl@Mikes-MacBook-Pro.local>
2020-11-04 17:44:27 -0800

    making progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
@@ -12,16 +11,19 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import RenderChatRoom from "./chatRoom"
 
 import {
   Link,
   Switch,
   Route,
 } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 const Home = () => <div>
   <li>Browser</li><li>Runs on local machine</li><li>React renders user interface</li><li>React Router adds clickable links</li>
 </div>
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -55,13 +57,12 @@ export default function PermanentDrawerLeft() {
   const [rooms, setRooms] = useState([]);
 
   const getRooms = async () => {
-    const rooms = await axios.get(`api/rooms`)
+    const rooms = await axios.get(`/rooms`); // TODO romove axios : no need for axios as we don't need support of older browsers and we aren't concerned with setting timeout params and things of that nature
     setRooms(rooms.data);
   }
   // Get chatRooms on load
   useEffect(() => {
     getRooms();
-    console.log('called getrooms')
   }, []);
 
 
@@ -69,15 +70,13 @@ export default function PermanentDrawerLeft() {
     return <div>Something went wrong</div>;
   }
 
-
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" noWrap>
-            Permanent drawer
-          </Typography>
+            Dash Chat          </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
@@ -88,61 +87,32 @@ export default function PermanentDrawerLeft() {
         }}
         anchor="left"
       >
-
         <div className={classes.toolbar} />
         <Divider />
         <div>
           <List>
             {rooms.map((room: any) => {
               const { id, name } = room;
-              return <ListItem button key={id}>
-                <Link to="/">
-                  <ListItemText primary={name} />
-                </Link>
+              return <ListItem component={Link} to={`/rooms/${id}`} button key={id}>
+                <ListItemText primary={name} />
               </ListItem>
             })}
           </List>
         </div>
         <ul role="nav">
           <li><Link to="/about">about</Link></li>
-          {/* <li><Link to="/">home</Link></li> */}
+          <li><Link to="/">home</Link></li>
         </ul>
       </Drawer>
       <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Switch>
-          <Route exact path="/" component={Home}>
-          </Route>
-          <Route path='/book/:id' render={(props) => {
-            return (<div>book detail</div>)
-          }} />
+          <Route exact path="/" component={Home} />
+          <Route path='/rooms/:id' render={routerProps => <RenderChatRoom props={routerProps} />} />
           <Route exact path="/about">
             <div>about</div>
           </Route>
         </Switch>
-        <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
       </main>
     </div>
   );
